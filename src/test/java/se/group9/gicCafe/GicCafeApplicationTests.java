ba7bbb5388fbeb1852f02da935fac48fef4a38ea
PhalKimheng <phalkimheng555@gmail.com>
2023-04-24 22:29:53 +0700

    set-up string boot

---
+++ src/test/java/se/group9/gicCafe/GicCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.group9.gicCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GicCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
