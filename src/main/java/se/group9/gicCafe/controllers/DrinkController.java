05587f2dc83fc511defa845df4e25f33c4324738
PHENG <male50515.2099@gmail.com>
2023-05-24 08:12:34 +0700

    no idea what was changed as I cant change to branhc unless commit or...

--- src/main/java/se/group9/gicCafe/controllers/DrinkController.java
+++ src/main/java/se/group9/gicCafe/controllers/DrinkController.java
@@ -27,93 +27,88 @@ import se.group9.gicCafe.repository.OrderRepository;
 
 @Controller
 public class DrinkController {
-    @GetMapping("/")
-    String h() {
-        return "fragments/Drink_FoodData";
-    }
+    // @Autowired
+    // DrinkRepository drinkRepository;
 
-    @Autowired
-    DrinkRepository drinkRepository;
+    // @Autowired
+    // CategoryRepository categoryRepository;
 
-    @Autowired
-    CategoryRepository categoryRepository;
+    // @Autowired
+    // DrinkSizeRepository drinkSizeRepository;
 
-    @Autowired
-    DrinkSizeRepository drinkSizeRepository;
+    // @Autowired
+    // OrderRepository orderRepository;
 
-    @Autowired
-    OrderRepository orderRepository;
+    // @Autowired
+    // OrderDetailRepository orderDetailRepository;
 
-    @Autowired
-    OrderDetailRepository orderDetailRepository;
+    // @GetMapping("drink-selection-order-info")
+    // public String getDrinkSelectionView(Model model) {
+    //     model.addAttribute("orderDetails", orderDetailRepository.findAll());
+    //     model.addAttribute("categoryList", categoryRepository.findAll());
+    //     model.addAttribute("drinkList", drinkRepository.findAll());
+    //     return "drinkSelection_orderInfo";
+    // }
 
-    @GetMapping("drink-selection-order-info")
-    public String getDrinkSelectionView(Model model) {
-        model.addAttribute("orderDetails", orderDetailRepository.findAll());
-        model.addAttribute("categoryList", categoryRepository.findAll());
-        model.addAttribute("drinkList", drinkRepository.findAll());
-        return "drinkSelection_orderInfo";
-    }
+    // @PostMapping("drink-selection-order-info")
+    // public String GetdrinkData(Model model,
+    //         @RequestParam(value = "id") long ctg_id) {
 
-    @PostMapping("drink-selection-order-info")
-    public String GetdrinkData(Model model,
-            @RequestParam(value = "id") long ctg_id) {
+    //     model.addAttribute("orderDetails", orderDetailRepository.findAll());
+    //     model.addAttribute("categoryList", categoryRepository.findAll());
+    //     if (ctg_id == 0) {
+    //         model.addAttribute("drinkList", drinkRepository.findAll());
+    //         return "fragments/Drink_FoodData :: drink_food";
+    //     }
+    //     model.addAttribute("drinkList", categoryRepository.findById(ctg_id).get().getDrink());
+    //     return "fragments/Drink_FoodData :: drink_food";
+    // }
 
-        model.addAttribute("orderDetails", orderDetailRepository.findAll());
-        model.addAttribute("categoryList", categoryRepository.findAll());
-        if (ctg_id == 0) {
-            model.addAttribute("drinkList", drinkRepository.findAll());
-            return "fragments/Drink_FoodData :: drink_food";
-        }
-        model.addAttribute("drinkList", categoryRepository.findById(ctg_id).get().getDrink());
-        return "fragments/Drink_FoodData :: drink_food";
-    }
+    // @GetMapping("/add-drink")
+    // String addDrink(Model model) {
+    //     model.addAttribute("categoryList", categoryRepository.findAll());
 
-    @GetMapping("/add-drink")
-    String addDrink(Model model) {
-        model.addAttribute("categoryList", categoryRepository.findAll());
+    //     // model.addAttribute("drinkSizes", drinkSizeList);
 
-        // model.addAttribute("drinkSizes", drinkSizeList);
+    //     model.addAttribute("drink", new Drink(12, null, null, null, null, null, null, null));
+    //     return "newDrink";
+    // }
 
-        model.addAttribute("drink", new Drink(12, null, null, null, null, null, null, null));
-        return "newDrink";
-    }
+    // @PostMapping("/add-drink")
+    // public String addDrink(@ModelAttribute("drink") Drink drink,
+    //         @RequestParam("image") MultipartFile multipartFile,
+    //         @RequestParam double sizeR,
+    //         @RequestParam double sizeL,
+    //         @RequestParam double sizeG,
+    //         Model model) throws IOException {
 
-    @PostMapping("/add-drink")
-    public String addDrink(@ModelAttribute("drink") Drink drink,
-            @RequestParam("image") MultipartFile multipartFile,
-            @RequestParam double sizeR,
-            @RequestParam double sizeL,
-            @RequestParam double sizeG,
-            Model model) throws IOException {
+    //     String fileName = multipartFile.getOriginalFilename();
+    //     System.out.println(fileName);
+    //     String uploadDir = "user-photos/Drink";
 
-        String fileName = multipartFile.getOriginalFilename();
-        System.out.println(fileName);
-        String uploadDir = "user-photos/Drink";
+    //     FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+    //     drink.setImagePath(uploadDir + "/" + fileName);
+    //     drinkRepository.saveAndFlush(drink);
 
-        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-        drink.setImagePath(uploadDir + "/" + fileName);
-        drinkRepository.saveAndFlush(drink);
+    //     List<DrinkSize> drinkSizeList = new ArrayList<>(Arrays.asList(
+    //             new DrinkSize(0, "R", sizeR, drink, null),
+    //             new DrinkSize(0, "L", sizeR, drink, null),
+    //             new DrinkSize(0, "G", sizeR, drink, null)));
 
-        List<DrinkSize> drinkSizeList = new ArrayList<>(Arrays.asList(
-                new DrinkSize(0, "R", sizeR, drink, null),
-                new DrinkSize(0, "L", sizeR, drink, null),
-                new DrinkSize(0, "G", sizeR, drink, null)));
+    //     drinkSizeRepository.saveAllAndFlush(drinkSizeList);
+    //     return "redirect:/add-drink";
+    // }
 
-        drinkSizeRepository.saveAllAndFlush(drinkSizeList);
-        return "redirect:/add-drink";
-    }
+    // @GetMapping("/add-category")
+    // public String addCategoryScree(Model model) {
+    //     model.addAttribute("ctg", new DrinkCategory());
+    //     // model.addAttribute("isValid", true);
+    //     return "newCategory";
+    // }
 
-    @GetMapping("/add-category")
-    public String addCategoryScree(Model model) {
-        model.addAttribute("ctg", new DrinkCategory());
-        // model.addAttribute("isValid", true);
-        return "newCategory";
-    }
-
-    @PostMapping("/add-category")
-    public String addCategor(@ModelAttribute("ctg") DrinkCategory drinkCategory, Model model) {
-        // model.addAttribute("isValid", false);
-        return "redirect:/add-category";
-    }
+    // @PostMapping("/add-category")
+    // public String addCategor(@ModelAttribute("ctg") DrinkCategory drinkCategory, Model model) {
+    //     // model.addAttribute("isValid", false);
+    //     return "redirect:/add-category";
+    // }
 }
